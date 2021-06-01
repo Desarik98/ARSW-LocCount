b9fac9cf5514e6bf83851afba168b1a88bb02685
Daniel Mejia <daniel.mejia@mail.escuelaing.edu.co>
2021-06-01 15:14:34 -0500

    "Finalizado contar lineas por phy"

--- src/main/java/edu/escuelaing/arsw/App.java
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -1,13 +1,21 @@
 package edu.escuelaing.arsw;
 
+import edu.escuelaing.arsw.LocCount.PhyCount;
+
+import java.io.FileNotFoundException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        String typeOfCount= args[0];
+        String fileName = args[1];
+        if(typeOfCount.equals("phy")){
+            PhyCount phyCount = new PhyCount();
+            System.out.println(phyCount.numberOfLines(fileName));
+        }
     }
 }
---
+++ src/main/java/edu/escuelaing/arsw/LocCount/PhyCount.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arsw.LocCount;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class PhyCount {
+    public long numberOfLines(String fileName){
+        Path path = Paths.get(fileName);
+        long numLines = 0;
+        try(Stream<String> stream = Files.lines(path)){
+             numLines = stream.count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numLines;
+    }
+}
