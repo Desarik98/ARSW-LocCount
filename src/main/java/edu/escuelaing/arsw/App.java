a6b7c7dcccc430b6ffc3dc13d1b175ad0db8e3d3
Daniel Mejia <daniel.mejia@mail.escuelaing.edu.co>
2021-06-01 20:04:11 -0500

    Finalizado lectura por wildcard

--- src/main/java/edu/escuelaing/arsw/App.java
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -2,19 +2,51 @@ package edu.escuelaing.arsw;
 
 import edu.escuelaing.arsw.LocCount.LocCount;
 import edu.escuelaing.arsw.LocCount.PhyCount;
+import java.io.IOException;
+import java.nio.file.*;
+
 
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
         String typeOfCount= args[0];
         String fileName = args[1];
+        int index = fileName.indexOf("*");
+        if(index > 0){
+            LocDir(fileName,index,typeOfCount);
+        } else {
+            System.out.println(LocFile(fileName,typeOfCount));
+        }
+    }
+
+    public static void LocDir(String fileName,int index,String typeOfCount) throws IOException {
+        String path = fileName.substring(0,index);
+        String glob = fileName.substring(index+2);
+        Path dir = FileSystems.getDefault().getPath(path);
+        DirectoryStream<Path> stream = Files.newDirectoryStream(dir,"*.{"+glob+"}");
+        for (Path i:stream){
+            if(typeOfCount.equals("phy")){
+                PhyCount phyCount = new PhyCount();
+                System.out.println(phyCount.numberOfLines(dir+"/"+i.getFileName().toString()));
+            }
+            else{
+                LocCount locCount = new LocCount();
+                System.out.println(locCount.numberOfLines(dir+"/"+i.getFileName().toString()));
+            }
+        }
+        stream.close();
+    }
+
+    public static long LocFile(String fileName,String typeOfCount){
+        long numLines;
         if(typeOfCount.equals("phy")){
             PhyCount phyCount = new PhyCount();
-            System.out.println(phyCount.numberOfLines(fileName));
+            numLines = phyCount.numberOfLines(fileName);
         }
         else{
             LocCount locCount = new LocCount();
-            System.out.println(locCount.numberOfLines(fileName));
+            numLines = locCount.numberOfLines(fileName);
         }
+        return numLines;
     }
 }
